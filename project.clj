633c7c688d2614bde6113af015686a5ac3271676
Simon Thörnqvist <simon@plug-n-trade.com>
2017-03-12 00:25:20 +0100

    log & spit to markdown file

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]]
+  :dependencies [[org.clojure/clojure "1.8.0"] [me.raynes/conch "0.8.0"]]
   :main ^:skip-aot changelog.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
--- src/changelog/core.clj
+++ src/changelog/core.clj
@@ -1,5 +1,23 @@
 (ns changelog.core
-  (:gen-class))
+  (:require [me.raynes.conch :refer [programs with-programs let-programs] :as sh]))
+(programs git)
+
+(def log-args ["--pretty=format:" "\"%h-%an-%ar-%s\""])
+
+(defn prepare-log-line [line]
+  (clojure.string/split (clojure.string/replace line #"\"" "") #"-"))
+
+(defn git-log
+  [dir]
+  (git "log" (clojure.string/join log-args) {:seq true :dir dir}))
+
+(defn markdown-format
+  [[sha1 author time commit-msg]]
+  (str "[" sha1 "]" "(http://github.com)" " " "**"author"**" " " "_"time"_" "\n> " commit-msg "\n"))
+
+(defn write-changelog
+  [lines]
+  (spit "changelog.md" (clojure.string/join "\n" lines)))
 
 (defn -main
   "I don't do a whole lot ... yet."
