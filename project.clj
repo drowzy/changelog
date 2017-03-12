3f476a8b45667038aec7e54445786ce41881183d
Simon Thörnqvist <simon@plug-n-trade.com>
2017-03-12 16:58:23 +0100

    add cli-tools dep

--- project.clj
+++ project.clj
@@ -3,7 +3,9 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"] [me.raynes/conch "0.8.0"]]
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [org.clojure/tools.cli "0.3.5"]
+                 [me.raynes/conch "0.8.0"]]
   :main ^:skip-aot changelog.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
